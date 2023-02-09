42427842e1bba4e82a6c1677744a0814bfb207b3
Mark Chalenko <mark.chalenko@rndx2.com>
2023-02-09 13:45:10 +0300

    feat: laravel has been updated to version 9.51.0

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,8 @@
 
 namespace Database\Seeders;
 
-
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //\App\Models\User::factory(10)->create();
-        /*DB::table('companies')->insert([
-            'name' => "Yoko",
-            'email' => "test@gmail.com",
-            //'password' => "password",
-        ]);*/
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,20 +13,6 @@ use App\Http\Controllers\UserController;
 |
 */
 
-Route::get('/', [\App\Http\Controllers\IndexController::class, 'showHomePage'])->name('home');
-
-Route::middleware("auth")->group(function(){
-    Route::get('/logout', [\App\Http\Controllers\AuthController::class, 'logout'])->name('logout');
-
-    Route::post('/image/upload', [\App\Http\Controllers\ImageController::class, 'imageUpload'])->name('imageUpload');
-
-    Route::resource('workers', UserController::class);
-});
-
-Route::middleware("guest")->group(function(){
-    Route::get('/login', [\App\Http\Controllers\IndexController::class, 'showLoginForm'])->name('login');
-    Route::post('/login_process', [\App\Http\Controllers\AuthController::class, 'login'])->name('login_process');
-
-    Route::get('/register', [\App\Http\Controllers\IndexController::class, 'showRegisterForm'])->name('register');
-    Route::post('/register_process', [\App\Http\Controllers\AuthController::class, 'register'])->name('register_process');
+Route::get('/', function () {
+    return view('welcome');
 });
